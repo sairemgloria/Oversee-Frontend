dede3ae604d5383c2b751a4f19246260e3dc249b
sairemgloria <sairemgloria1999@gmail.com>
2025-03-12 13:11:04 +0800

    Fix .gitignore, update adminDepartmentStore.js & adminStore.js

--- src/stores/admin/adminDepartmentStore.js
+++ src/stores/admin/adminDepartmentStore.js
@@ -2,8 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useAdminDepartmentStore = defineStore(
   "adminDepartmentStore",
