c7bfe5f3bd305cf6abc5c160fc95c27328c885e0
sairemgloria <sairemgloria1999@gmail.com>
2025-03-26 23:48:13 +0800

    Added AttendanceView.vue & DepartmentView.vue for admin view.

---
+++ src/views/admin/AttendanceView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Breadcrumb from "@/components/admin/Breadcrumb.vue";
+</script>
+
+<template>
+  <div class="p-6">
+    <!-- Employees View Header -->
+    <div class="mb-4">
+      <h1 class="text-3xl font-bold flex items-center">Attendance View</h1>
+    </div>
+
+    <Breadcrumb />
+
+    <hr class="my-6" />
+  </div>
+</template>
+
+<style scoped>
+</style>
